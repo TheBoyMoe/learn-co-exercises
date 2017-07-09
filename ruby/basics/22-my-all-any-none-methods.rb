1a32cf266fe29a6b14226b5b3851e9f24a5a69f8
theboymo <github email address>
2017-07-09 17:16:26 +0100

    implemtation of select, detect and reject methods

--- ruby/basics/22-my-all-any-none-methods.rb
+++ ruby/basics/22-my-all-any-none-methods.rb
@@ -1,5 +1,6 @@
 # dummy data
 names = ['Tim', 'Tom', 'Simon', 'Jack', 'John']
+no_ts = ['Simon', 'Jack', 'John', 'Mick']
 nums = [1,2,3,4,5,6,7,8]
 even = [2,4,6,8]
 odd = [1,3,5,7]
@@ -76,3 +77,98 @@ result = my_any?(odd) do |val|
 end
 
 puts "any even? #{result}"
+
+#### implementation of select? (filter) ###
+
+def my_select(collection)
+    if block_given?
+        i =  0
+        matches = []
+        while i < collection.length
+            result = yield(collection[i])
+            if result
+                matches << collection[i]
+            end
+            i += 1
+        end
+        matches
+    else
+        'No block given'
+    end
+end
+
+# test 
+result = my_select(names) do |name|
+    name.start_with?('T')
+end
+
+puts "start with t: #{result.inspect}"
+
+result = my_select(no_ts) do |name|
+    name.start_with?('T')
+end
+
+puts "no ts: #{result.inspect}"
+
+
+#### implementation of #reject - return all values which fail condition #####
+
+def my_reject(collection)
+    if block_given?
+        i =  0
+        rejects = []
+        while i < collection.length
+            result = yield(collection[i])
+            if !result
+                rejects << collection[i]
+            end
+            i += 1
+        end
+        rejects
+    else
+        'No block given'
+    end
+end
+
+# test
+result = my_reject(nums) do |num|
+    num % 2 == 0
+end
+
+puts nums.inspect
+puts "All odd numbers: #{result.inspect}"
+
+
+#### implementation of #detect - return the first matching element ####
+
+def my_detect(collection)
+    if block_given?
+        i =  0
+        match = []
+        while i < collection.length
+            result = yield(collection[i])
+            if result
+                match << collection[i]
+            end
+            i += 1
+        end
+        match.first
+    else
+        'No block given'
+    end
+end
+
+# test
+result = my_detect(names) do |name|
+    name.start_with?('J')
+end
+
+puts names.inspect
+puts "First match stating with 'J': #{result}"
+
+result = my_detect(no_ts) do |name|
+    name.start_with?('T')
+end
+
+puts no_ts.inspect
+puts "First match starting with 'T': #{result}"
