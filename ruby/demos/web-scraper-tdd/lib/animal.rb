bc6b9cb87376dfed0809ffbdba573297703521ad
theboymo <billfero@gmail.com>
2017-07-22 18:28:20 +0100

    refactor - move scraping code from animal constructor into dedicated AnimalScraper class

--- ruby/demos/web-scraper-tdd/lib/animal.rb
+++ ruby/demos/web-scraper-tdd/lib/animal.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require_relative './animal_scraper.rb' # req'd if loading file via irb
 
 class Animal
   attr_accessor :name, :kingdom, :phylum, :klass, :order, :family, :genus, :species
@@ -15,19 +16,34 @@ class Animal
 
   # constructor
   def self.new_from_wikipedia(url)
-    animal = Animal.new
-    # fetch data from wiki page
-    doc = Nokogiri::HTML(open(url))
-    animal.name = doc.search("h1#firstHeading").text
-    animal.kingdom = doc.search(".infobox a[href='/wiki/Animal']").text
-    animal.phylum =	doc.search(".infobox a[title=Chordate]").text
-    animal.klass =	doc.search(".infobox a[title=Mammal]").text
-    animal.order =	doc.search(".infobox a[title^=Even-toed]").text
-    animal.family =	doc.search(".infobox a[title=Hippopotamidae]").text
-    animal.genus =	doc.search(".infobox a[title^=Hippopotamus]").text
-    animal.species = doc.search("span.species").text
+    Animal.new.tap do |animal|
+      # fetch data from wiki page - refactored to AnimalScraper class
+      # doc = Nokogiri::HTML(open(url))
+      # animal.name = doc.search("h1#firstHeading").text
+      # animal.kingdom = doc.search(".infobox a[href='/wiki/Animal']").text
+      # animal.phylum =	doc.search(".infobox a[title=Chordate]").text
+      # animal.klass =	doc.search(".infobox a[title=Mammal]").text
+      # animal.order =	doc.search(".infobox a[title^=Even-toed]").text
+      # animal.family =	doc.search(".infobox a[title=Hippopotamidae]").text
+      # animal.genus =	doc.search(".infobox a[title^=Hippopotamus]").text
+      # animal.species = doc.search("span.species").text
+      properties = AnimalScraper.wikipedia_scraper(url)
+      properties.each do |k, v|
+        animal.send("#{k}=", v) # mass assignment
+      end
+    end
+  end
 
-    animal
+  # constructor - instantiate the obj using different sources
+  def self.new_from_file(path)
+    Animal.new.tap do |animal|
+      # configure animal obj and return
+    end
+  end
+
+  # finder method, e.g. find an animal by name
+  def self.find_by_name(name)
+    self.all.detect {|a| a.name == name}
   end
 
 end
--- ruby/demos/web-scraper-tdd/lib/animal_scraper.rb
+++ ruby/demos/web-scraper-tdd/lib/animal_scraper.rb
@@ -1,3 +1,17 @@
-class AnimalScrapper
+class AnimalScraper
 
+  def self.wikipedia_scraper(url)
+    doc = Nokogiri::HTML(open(url))
+
+    animal = {}
+    animal[:name] = doc.search("h1#firstHeading").text
+    animal[:kingdom] = doc.search(".infobox a[href='/wiki/Animal']").text
+    animal[:phylum] =	doc.search(".infobox a[title=Chordate]").text
+    animal[:klass] =	doc.search(".infobox a[title=Mammal]").text
+    animal[:order] =	doc.search(".infobox a[title^=Even-toed]").text
+    animal[:family] =	doc.search(".infobox a[title=Hippopotamidae]").text
+    animal[:genus] =	doc.search(".infobox a[title^=Hippopotamus]").text
+    animal[:species] = doc.search("span.species").text
+    animal
+  end
 end
