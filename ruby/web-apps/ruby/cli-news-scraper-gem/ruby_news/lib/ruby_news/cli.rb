796bc1aaca2451a6521ef045c1128b11cb5df9ed
theboymo <billfero@gmail.com>
2017-08-04 20:22:56 +0100

    allow the user to select the article from the cli

--- ruby/web-apps/ruby/cli-news-scraper-gem/ruby_news/lib/ruby_news/cli.rb
+++ ruby/web-apps/ruby/cli-news-scraper-gem/ruby_news/lib/ruby_news/cli.rb
@@ -7,6 +7,25 @@ class RubyNews::CLI
   end
 
   def call
+    input = ''
+    while input != 'exit'
+      puts "Type 'list' to list articles, 'exit' to exit"
+      puts "What would you like to do?"
+      input = gets.strip
+      case input
+      when 'list'
+        list_articles
+      when 'exit'
+        puts 'Goodbye'
+        break
+      else
+        # open the default application for the file
+        system("open #{@newsletter.articles[input.to_i - 1].url}")
+      end
+    end
+  end
+
+  def list_articles
     @newsletter.articles.each.with_index(1) do |article, i|
       puts "#{i}. #{article.title}"
     end
