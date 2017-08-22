315696f606b9553d13c3b7b1be7dcd521d62fab7
theboymo <billfero@gmail.com>
2017-08-22 18:33:08 +0100

    save data to the database

--- sqlite/08-roommate-finder-cli/config/environment.rb
+++ sqlite/08-roommate-finder-cli/config/environment.rb
@@ -5,3 +5,5 @@ Bundler.require
 require_all 'lib'
 
 require 'open-uri'
+
+DB = {:conn => SQLite3::Database.new('db/roommate.db')}
--- sqlite/08-roommate-finder-cli/lib/room.rb
+++ sqlite/08-roommate-finder-cli/lib/room.rb
@@ -2,6 +2,7 @@ class Room
   attr_accessor :title, :date_created, :price, :url
 
   def self.create_from_hash(hash)
+    # save each record in turn to the database
     self.new_from_hash(hash).save
   end
 
@@ -12,13 +13,34 @@ class Room
     room.date_created = hash[:date_created]
     room.price = hash[:price]
     room.url = hash[:url]
-    binding.pry
     room
   end
 
   def save
+    self.insert
+  end
+
+  def insert
     # save the instance to the database
-    puts 'saving to the database'
+    sql = <<-SQL
+      INSERT INTO rooms (title, date_created, price, url)
+      VALUES (?, ?, ?, ?)
+    SQL
+    DB[:conn].execute(sql, self.title, self.date_created, self.price, self.url)
+  end
+
+  # TODO
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS rooms (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        date_created TEXT,
+        price TEXT,
+        url TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
   end
 
 end
