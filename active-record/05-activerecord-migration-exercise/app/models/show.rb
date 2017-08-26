5cb26617a0edf9bf413da80999860dd534763135
theboymo <billfero@gmail.com>
2017-08-26 15:57:21 +0100

    active record migration exercise

---
+++ active-record/05-activerecord-migration-exercise/app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.order(rating: :desc).first.rating
+  end
+
+  def self.most_popular_show
+    Show.order(rating: :desc).first
+  end
+
+  def self.lowest_rating
+    Show.order(rating: :asc).first.rating
+  end
+
+  def self.least_popular_show
+    Show.order(rating: :asc).first
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where('rating > 5')
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
+
+end
