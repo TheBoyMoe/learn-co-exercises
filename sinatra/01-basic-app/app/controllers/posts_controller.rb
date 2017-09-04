f9b9694a4b2fb743e547de2217592bd7df9608cc
theboymo <billfero@gmail.com>
2017-09-04 12:49:14 +0100

    impl posts new and create actions

--- sinatra/01-basic-app/app/controllers/posts_controller.rb
+++ sinatra/01-basic-app/app/controllers/posts_controller.rb
@@ -1,20 +1,31 @@
 # controller should handle one thing, thus use a separate controller for posts, users, categories, tags, etc
 
+# the view has access to all of the instance variables defined in the instance controller
+# but that instance variable is only available within the controller where it's defined
+
+# Each controller should handle ONE resource and should
+# implement these 7 actions (follows the design pattern described by Roy Fielding)
+# 1. posts#index action #=> show all items
+# 2. posts#show action #=> show item
+# 3. posts#new action #=> allow user to create new item
+# 4. posts#create action => add new item
+# 5. posts#edit action => allow user to edit item
+# 6. posts#update action #=> update item
+# 7. posts#destroy action #=> delete item
+
+# Order of your actions matter, have more specific actions appear before more generic ones, e.g 'post#new' before 'posts#id'
+
+# we create dynamic routes using route variables - any variable starting with ':'
+# :id is a route variable -> way of passing a dynamic variable into your route
+# anything following '?' is part of the query string and (texhnically) not part of the url
+# (you can retrieve query string values from the params hash via their keys.)
+
+# all data submitted by a user will appear in params - weather it was submitted via
+# a url route variables, query parameters, form submission, etc. Route variables & query parameters end up as keys
 # any methods/functionality defined in ApplicationController is automatically available to PostsController
+
+
 class PostsController < ApplicationController
-
-  # the view has access to all of the instance variables defined in the instance controller
-  # but that instance variable is only available within the controller where it's defined
-
-  # Each controller should handle ONE resource and should
-  # implement these 7 actions (follows the design pattern described by Roy Fielding)
-  # 1. posts#index action #=> show all items
-  # 2. posts#show action #=> show item
-  # 3. posts#new action #=> allow user to create new item
-  # 4. posts#create action => add new item
-  # 5. posts#edit action => allow user to edit item
-  # 6. posts#update action #=> update item
-  # 7. posts#destroy action #=> delete item
 
   # posts#index action
   get '/posts' do
@@ -23,21 +34,45 @@ class PostsController < ApplicationController
     erb :'posts/index'
   end
 
-  # we create dynamic routes using route variables - any variable starting with ':'
-  # :id is a route variable -> way of passing a dynamic variable into your route
-  # anything following '?' is part of the query string and (texhnically) not part of the url
-  # (you can retrieve query string values from the params hash via their keys.)
+  # posts#new action
+  get '/posts/new' do
+    # render page that allows user to create a post
+    erb :'posts/new'
+  end
 
-  # all data submitted by a user will appear in params - weather it was submitted via
-  # a url route variables, query parameters, form submission, etc. Route variables & query parameters end up as keys
-
-  posts#show
+  # posts#show action
   get '/posts/:id' do
     # raise params.inspect #=> view the params entered by the user
     @post = Post.find(params[:id])
 
     erb :'posts/show'
   end
+
+  # action called by submitting the form
+  # posts#create action
+  post '/posts' do
+    # raise params.inspect #=> debug
+
+    # @post = Post.new
+    # @post.title = params[:title_field]
+    # @post.content = params[:content_field]
+
+    # if you use mass-assignment(via params hash) the name attribute values in
+    # your form MUST match the names of your property attributes defined in your migrations
+    @post = Post.new(params)
+    @post.save
+
+    redirect "/posts/#{@post.id}"
+  end
+
+  # posts#edit action
+
+
+  # posts#update action
+
+
+  # posts#destroy action
+
 
 
 
