c17de62841878633e1e1b1a0e41eeffe8941a12d
theboymo <billfero@gmail.com>
2017-09-07 14:54:25 +0100

    impl index, show, new and create actions

--- sinatra/10-sinatra-nested-forms-with-persistence/app.rb
+++ sinatra/10-sinatra-nested-forms-with-persistence/app.rb
@@ -7,19 +7,38 @@ module FormsLab
       erb :root
     end
 
-    get '/new' do
+    # /pirates#index action
+    get '/pirates' do
+      @pirates = Pirate.all
+
+      erb :'pirates/index'
+    end
+
+    # /pirates#new action - returns a form that allows user to add a pirate
+    get '/pirates/new' do
       erb :'pirates/new'
     end
 
-    post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |ship_hash|
-        Ship.new(ship_hash)
-      end
-      @ships = Ship.all
+    # /pirates#show action
+    get '/pirates/:id' do
+      @pirate = Pirate.find(params[:id])
 
       erb :'pirates/show'
+    end
+
+
+    # /pirates#create action - save the new pirate(and ships) to the database
+    post '/pirates' do
+      binding.pry
+      @pirate = Pirate.create(name: params[:pirate][:name], weight: params[:pirate][:weight], height: params[:pirate][:height])
+
+      params[:pirate][:ships].each do |ship_hash|
+        ship = Ship.create(name: ship_hash[:name], category: ship_hash[:category], booty: ship_hash[:booty])
+        ship.pirate = @pirate
+        ship.save
+      end
+
+      redirect :"/pirates/#{@pirate.id}"
     end
 
   end
