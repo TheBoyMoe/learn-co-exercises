d8c49b0d437ef8ad351cadc9384f6d62873bc545
theboymo <billfero@gmail.com>
2017-11-08 10:17:58 +0000

    rails form_for lab example

---
+++ 08-rails/demo-apps/09-rails-form_for-lab/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+
+  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+end
