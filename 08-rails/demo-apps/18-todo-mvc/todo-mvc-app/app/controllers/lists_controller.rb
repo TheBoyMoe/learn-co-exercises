fadc4cc93f3f9ae471fb55503e36728070444cf2
theboymo <billfero@gmail.com>
2017-11-12 12:22:43 +0000

    impl basic list index and show actions, remove static controller

--- 08-rails/demo-apps/18-todo-mvc/todo-mvc-app/app/controllers/lists_controller.rb
+++ 08-rails/demo-apps/18-todo-mvc/todo-mvc-app/app/controllers/lists_controller.rb
@@ -1,2 +1,16 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+
+  end
+
+  private
+    def set_list
+      @list = List.find_by(id: params[:id])
+    end
 end
