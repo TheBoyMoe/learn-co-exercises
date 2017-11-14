548404501a3124f75a77fbd8706f173cc77de55b
theboymo <billfero@gmail.com>
2017-11-14 20:10:15 +0000

    review forms and basic associations

---
+++ 08-rails/demo-apps/21-displaying-associations-rails-lab/app/controllers/artists_controller.rb
@@ -0,0 +1,52 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to @artist
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @artist.update(artist_params)
+    if @artist.save
+      redirect_to @artist
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    flash[:notice] = "Artist deleted."
+    redirect_to artists_path
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+end
