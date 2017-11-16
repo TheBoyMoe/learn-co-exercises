170e2cb690392a81b222a831d6a482e3396a2004
theboymo <billfero@gmail.com>
2017-11-16 20:18:23 +0000

    review has many relationship in rails

---
+++ 08-rails/demo-apps/23-displaying-has-many-through-rails-lab/app/controllers/doctors_controller.rb
@@ -0,0 +1,15 @@
+class DoctorsController < ApplicationController
+  before_action :set_doctor, only: [:show]
+
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+  end
+
+  private
+    def set_doctor
+      @doctor = Doctor.find_by(id: params[:id])
+    end
+end
