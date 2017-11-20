3febfde5a01ce553abf45ce271999d7ba401936a
theboymo <billfero@gmail.com>
2017-11-20 19:21:38 +0000

    create user with email field, redirect to show page if successful

--- 08-rails/demo-apps/25-nested-forms-lab/nested-form/app/controllers/users_controller.rb
+++ 08-rails/demo-apps/25-nested-forms-lab/nested-form/app/controllers/users_controller.rb
@@ -1,13 +1,34 @@
 class UsersController < ApplicationController
-  def new
-  end
+  before_action :set_user, only: [:show]
 
   def show
   end
 
   def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.team = Team.find_or_create_by(name: 'The Avengers')
+    binding.pry
+    if @user.save
+      redirect_to @user # => /users/:id
+    else
+      render :new
+    end
   end
+
+  private
+    def set_user
+      @user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
