320cf088ee206899f6afcabfda527bc389ea7371
theboymo <billfero@gmail.com>
2017-11-23 14:51:46 +0000

    rendering partials and locals exercise

---
+++ 08-rails/demo-apps/30-render-partials-locals-and seach-lab/app/models/student.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  hometown   :string
+#  birthday   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Student < ActiveRecord::Base
+  has_many :classroom_students
+  has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    return Student.all if !query || query.empty?
+    Student.all.select do |student|
+      student if student.name.downcase.match query
+    end
+  end
+
+end
