e40360efe6f6953348a1dc21926536bb3b971d80
theboymo <billfero@gmail.com>
2017-12-03 21:10:56 +0000

    impl nested route to create list items

--- 08-rails/demo-apps/18-todo-mvc/todo-mvc-app/config/routes.rb
+++ 08-rails/demo-apps/18-todo-mvc/todo-mvc-app/config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
 
-  resources :lists
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # items are always created in the context of a list, so create them as a nested resource
+  resources :lists do
+    resources :items, only: [:create]
+  end
+
+  
+  # list_items  POST   /lists/:list_id/items(.:format) items#create
+
+
+  # lists       GET    /lists(.:format)                lists#index
+  #             POST   /lists(.:format)                lists#create
+  # new_list    GET    /lists/new(.:format)            lists#new
+  # edit_list   GET    /lists/:id/edit(.:format)       lists#edit
+  # list        GET    /lists/:id(.:format)            lists#show
+  #             PATCH  /lists/:id(.:format)            lists#update
+  #             PUT    /lists/:id(.:format)            lists#update
+  #             DELETE /lists/:id(.:format)            lists#destroy
+
 
   root 'lists#index'
 end
