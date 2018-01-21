041cb7aa57f0b5c7a2cbcb50e8589214f18a1913
theboymo <billfero@gmail.com>
2018-01-21 21:06:42 +0000

    create new posts via the form element

--- 08-rails/review/02-rails-form_tag-readme/app/controllers/posts_controller.rb
+++ 08-rails/review/02-rails-form_tag-readme/app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    @post = Post.new(title: params[:post][:title], description: params[:post][:description])
+    if @post.save
+      redirect_to posts_path
+    else
+      redirect_to new_posts_path
+    end
+  end
+
 end
--- 08-rails/review/02-rails-form_tag-readme/spec/features/post_spec.rb
+++ 08-rails/review/02-rails-form_tag-readme/spec/features/post_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 describe 'new post' do
+	it "ensures that the form route works with the /new action" do
+		visit new_post_path
+		expect(page.status_code).to eq(200)
+	end
 
+	it "renders HTML in the '/new' template" do
+		visit new_post_path
+		expect(page).to have_content('Post Form')
+	end
+
+	it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+		visit new_post_path
+		fill_in 'post_title', with: 'Leverage agile frameworks'
+		fill_in 'post_description',  with: 'Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+		click_on 'Submit Post'
+
+		expect(page.current_path).to eq(posts_path) # index page
+		expect(page).to have_content('Leverage agile frameworks')
+		expect(page).to have_content('Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.')
+	end
 end
