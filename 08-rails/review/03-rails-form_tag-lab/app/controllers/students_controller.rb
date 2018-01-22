557cf4453c95b9ade97cbb113a003907424b44c2
theboymo <billfero@gmail.com>
2018-01-22 13:44:16 +0000

    initial commit -f form_tag lab

---
+++ 08-rails/review/03-rails-form_tag-lab/app/controllers/students_controller.rb
@@ -0,0 +1,20 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: [:show]
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+end
