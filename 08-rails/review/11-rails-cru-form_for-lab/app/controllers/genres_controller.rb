6f6a2cb61f7d36450fd2828e08ca4cf01f9f0968
theboymo <billfero@gmail.com>
2018-01-24 11:21:12 +0000

    impl genres routes, controller and views

--- 08-rails/review/11-rails-cru-form_for-lab/app/controllers/genres_controller.rb
+++ 08-rails/review/11-rails-cru-form_for-lab/app/controllers/genres_controller.rb
@@ -1,26 +1,35 @@
 class GenresController < ApplicationController
-
-	def index
-
-	end
+	before_action :set_genre, only: [:show, :edit, :update]
 
 	def show
-
 	end
 
 	def new
-
+		@genre = Genre.new
 	end
 
 	def create
-
+		@genre = Genre.new(genre_params)
+		if @genre.save
+			redirect_to genre_path(@genre)
+		end
 	end
 
 	def edit
-
 	end
 
 	def update
-
+		if @genre.update(genre_params)
+			redirect_to genre_path(@genre)
+		end
 	end
+
+	private
+		def set_genre
+			@genre = Genre.find(params[:id])
+		end
+
+		def genre_params
+			params.require(:genre).permit(:name)
+		end
 end
