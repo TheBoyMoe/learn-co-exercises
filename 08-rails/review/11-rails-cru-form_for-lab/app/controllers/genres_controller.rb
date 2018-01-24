3d4dfad78473dd50b63c36615a21978e7c531034
theboymo <billfero@gmail.com>
2018-01-24 09:28:36 +0000

    add resource routes for artists, genres and songs. Add controller files and stub actions

---
+++ 08-rails/review/11-rails-cru-form_for-lab/app/controllers/genres_controller.rb
@@ -0,0 +1,26 @@
+class GenresController < ApplicationController
+
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+end
---
+++ 08-rails/review/11-rails-cru-form_for-lab/app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+end
