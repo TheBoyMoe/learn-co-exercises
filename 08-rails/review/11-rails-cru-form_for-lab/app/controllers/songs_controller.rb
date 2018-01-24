e85bd70959b8ab2adae43d1828be2790bb5cb74e
theboymo <billfero@gmail.com>
2018-01-24 12:15:15 +0000

    impl songs views and controller acions - passing specs

--- 08-rails/review/11-rails-cru-form_for-lab/app/controllers/songs_controller.rb
+++ 08-rails/review/11-rails-cru-form_for-lab/app/controllers/songs_controller.rb
@@ -1,19 +1,24 @@
 class SongsController < ApplicationController
+	before_action :set_song, only: [:show, :edit, :update]
 
 	def index
-
+		@songs = Song.all
 	end
 
 	def show
-
+		@artist = @song.artist
+		@genre = @song.genre
 	end
 
 	def new
-
+		@song = Song.new
 	end
 
 	def create
-
+		@song = Song.new(song_params)
+		if @song.save
+			redirect_to song_path(@song)
+		end
 	end
 
 	def edit
@@ -21,7 +26,17 @@ class SongsController < ApplicationController
 	end
 
 	def update
-
+		if @song.update(song_params)
+			redirect_to song_path(@song)
+		end
 	end
 
+	private
+		def set_song
+			@song = Song.find(params[:id])
+		end
+
+		def song_params
+			params.require(:song).permit(:name, :artist_id, :genre_id)
+		end
 end
