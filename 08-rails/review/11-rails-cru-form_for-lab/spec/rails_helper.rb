e1770f9482a067a3ece25365aa886e99a94cf625
theboymo <billfero@gmail.com>
2018-01-24 11:24:23 +0000

    update rails helper to record spec failures

--- 08-rails/review/11-rails-cru-form_for-lab/spec/rails_helper.rb
+++ 08-rails/review/11-rails-cru-form_for-lab/spec/rails_helper.rb
@@ -62,4 +62,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+	#
+
+  config.example_status_persistence_file_path = 'failures.txt'
 end
