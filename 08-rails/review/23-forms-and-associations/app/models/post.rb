d99cadef1a29d46f6c95de3a418d2aebb3d74e12
theboymo <billfero@gmail.com>
2018-01-28 21:00:55 +0000

    allow users to select the category from a dowp down list

--- 08-rails/review/23-forms-and-associations/app/models/post.rb
+++ 08-rails/review/23-forms-and-associations/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :category
 
+  # virtual attributes
   # called when ever Post is initialized with a 'category_name' field, 'posts#create'
   def category_name=(name)
     self.category = Category.find_or_create_by(name: name)
