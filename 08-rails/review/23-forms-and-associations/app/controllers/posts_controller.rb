38fb4f728dcbe193cc95800f8cbdf538086c649d
theboymo <billfero@gmail.com>
2018-01-28 20:13:38 +0000

    impl basic form with posts create action

--- 08-rails/review/23-forms-and-associations/app/controllers/posts_controller.rb
+++ 08-rails/review/23-forms-and-associations/app/controllers/posts_controller.rb
@@ -13,6 +13,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Post successfully created"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -24,5 +31,9 @@ class PostsController < ApplicationController
   private
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content, :category_id)
     end
 end
