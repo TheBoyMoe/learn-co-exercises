5aec15d9ee92a1c907f9a51f57f8bf52455020bc
theboymo <billfero@gmail.com>
2018-01-29 21:05:26 +0000

    initial commit - nested forms lab

---
+++ 08-rails/review/21-30/26-basic-nested-forms-lab/app/controllers/recipes_controller.rb
@@ -0,0 +1,16 @@
+class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+  end
+end
---
+++ 08-rails/review/21-30/26-basic-nested-forms-lab/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :recipes
+
+  root to: 'recipes#index'
+end
