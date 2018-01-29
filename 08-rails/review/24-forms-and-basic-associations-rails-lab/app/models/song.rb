2f792cf88e89781ad56dd03fce0f5bd819c2a8bb
theboymo <billfero@gmail.com>
2018-01-29 14:52:07 +0000

    impl song methods, song specs passing

--- 08-rails/review/24-forms-and-basic-associations-rails-lab/app/models/song.rb
+++ 08-rails/review/24-forms-and-basic-associations-rails-lab/app/models/song.rb
@@ -2,5 +2,41 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name
+    self.genre.name
+  end
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  # return the contents of each note as an array
+  def note_contents
+    self.notes.map(&:content)
+  end
+
+  # add each note to the song instance if it has not already
+  def note_contents=(notes)
+    notes.each do |content|
+      if content && !content.empty?
+        note = Note.find_or_create_by(content: content)
+        unless self.notes.include? note
+          self.notes << note
+        end
+      end
+    end
+  end
+
 end
 
