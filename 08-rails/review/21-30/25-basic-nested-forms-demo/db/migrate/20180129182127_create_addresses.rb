200748f1a58c4d38621a915eadb1d02043225092
theboymo <billfero@gmail.com>
2018-01-29 18:23:56 +0000

    create the apps data model

---
+++ 08-rails/review/21-30/25-basic-nested-forms-demo/db/migrate/20180129182127_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :addresses do |t|
+      t.string :street_address_1
+      t.string :street_address_2
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.string :address_type
+
+      t.timestamps
+    end
+  end
+end
