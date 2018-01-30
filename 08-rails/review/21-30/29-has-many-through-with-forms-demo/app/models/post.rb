a5e52ff3210393dde77a880cc6aefa1af6124aea
theboymo <billfero@gmail.com>
2018-01-30 20:44:08 +0000

    prevent users from adding duplicate categories through overriding categories_attributes= method

--- 08-rails/review/21-30/29-has-many-through-with-forms-demo/app/models/post.rb
+++ 08-rails/review/21-30/29-has-many-through-with-forms-demo/app/models/post.rb
@@ -4,4 +4,16 @@ class Post < ApplicationRecord
 
 	# enable creating new categories as nested attributes of the post model
 	accepts_nested_attributes_for :categories
+
+	# override categories_attributes= method so duplicates can not be defined NOT CASE SENSITIVE
+	def categories_attributes=(category_attributes)
+		category_attributes.values.each do |category_attribute|
+			if category_attribute[:name].present? # do not add blank categories
+				category = Category.find_or_create_by(name: category_attribute[:name])
+				unless self.categories.include?(category) #only add categories to the post which are missing
+					self.post_categories.build(category: category)
+				end
+			end
+		end
+	end
 end
