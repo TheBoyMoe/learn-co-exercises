8acebcb7dfc67313dd7604166a5b680f1141b4c7
theboymo <billfero@gmail.com>
2018-01-30 15:07:02 +0000

    impl views and controllers

--- 08-rails/review/21-30/27-has-many-through-demo/config/routes.rb
+++ 08-rails/review/21-30/27-has-many-through-demo/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :posts, only: [:show, :create, :new, :index]
+	resources :users, only: [:show, :create, :new, :index]
 end
