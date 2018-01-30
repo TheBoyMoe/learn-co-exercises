165bc40c6c0052486ef6ce903335e409f898daa0
theboymo <billfero@gmail.com>
2018-01-30 19:39:34 +0000

    impl basic creation/edit of posts

---
+++ 08-rails/review/21-30/29-has-many-through-with-forms-demo/app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update]
+
+	def index
+		@posts = Post.all
+	end
+
+	def show
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to post_path(@post)
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to post_path(@post)
+		else
+			render :edit
+		end
+	end
+	private
+		def set_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :content)
+		end
+end
