b6c5175fe2e34fa3dc62609544f141a056226e1a
theboymo <billfero@gmail.com>
2018-01-30 18:23:39 +0000

    complete displaying has_many through lab

--- 08-rails/review/21-30/28-displaying-has-many-through-rails-lab/config/routes.rb
+++ 08-rails/review/21-30/28-displaying-has-many-through-rails-lab/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :doctors
-  resources :patients
+  resources :doctors, only: [:show, :index]
+  resources :patients, only: [:show, :index]
   resources :appointments, only: [:show]
-
+  root to: 'doctors#index'
 end
--- 08-rails/spec_examples/14-various-view-specs.rb
+++ 08-rails/spec_examples/14-various-view-specs.rb
@@ -94,3 +94,27 @@ describe "artists", type:  :feature do
 		expect(page).to have_content("2 songs")
 	end
 end
+
+
+# review-28 spec/features/appointmetns_apc.rb
+describe "appointments", type:  :feature do
+	before do
+		@hawkeye = Doctor.create({name: "Hawkeye Pierce", department: "Surgery"})
+		@homer = Patient.create({name: "Homer Simpson", age:38})
+		@appointment = Appointment.create({appointment_datetime: DateTime.new(2016, 03, 15, 18, 00, 0), patient: @homer, doctor: @hawkeye})
+	end
+
+	it "should display an appointment's doctor" do
+		visit appointment_path(@appointment)
+		expect(page).to have_link("Hawkeye Pierce", href: doctor_path(@hawkeye))
+	end
+
+	it "should display an appointment's patient" do
+		visit appointment_path(@appointment)
+		expect(page).to have_link("Homer Simpson", href: patient_path(@homer))
+	end
+
+	it "should not have an index page" do
+		expect {visit('/appointments')}.to raise_error(ActionController::RoutingError)
+	end
+end
