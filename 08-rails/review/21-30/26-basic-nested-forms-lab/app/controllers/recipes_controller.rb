63e6cec5f6f1cbd0a4d36fa0906cf1b023785bb2
theboymo <billfero@gmail.com>
2018-01-30 13:24:21 +0000

    complete nested forms lab

--- 08-rails/review/21-30/26-basic-nested-forms-lab/app/controllers/recipes_controller.rb
+++ 08-rails/review/21-30/26-basic-nested-forms-lab/app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
+
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def index
@@ -9,8 +10,36 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    # add two ingredient fields
+    2.times {@recipe.ingredients.build}
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
+
+  def edit
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(
+      :title,
+          ingredients_attributes: [:id, :name, :quantity]
+      )
+    end
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
 end
