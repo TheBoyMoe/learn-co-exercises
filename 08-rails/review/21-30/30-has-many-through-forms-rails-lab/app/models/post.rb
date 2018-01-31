cf2e5019937a385d1df5ce40d39e194dc3f9ff10
theboymo <billfero@gmail.com>
2018-01-31 14:49:39 +0000

    allow users to add a category to a post, both new and existing

--- 08-rails/review/21-30/30-has-many-through-forms-rails-lab/app/models/post.rb
+++ 08-rails/review/21-30/30-has-many-through-forms-rails-lab/app/models/post.rb
@@ -3,4 +3,19 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories #=> post.categories (all categories assigned to the post)
   has_many :comments
   has_many :users, through: :comments #=> post.users (all users who have commented on the post)
+
+  # enable creating new categories as nested attributes of the post model
+  accepts_nested_attributes_for :categories
+
+  # override categories_attributes= method so duplicates can not be defined NOT CASE SENSITIVE
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute[:name].present? # do not add blank categories
+        category = Category.find_or_create_by(name: category_attribute[:name])
+        unless self.categories.include?(category) #only add categories to the post which are missing
+          self.post_categories.build(category: category)
+        end
+      end
+    end
+  end
 end
