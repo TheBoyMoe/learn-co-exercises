4d429e4a21ba0a8a4feb63a5f34709b63c2dc8a8
theboymo <billfero@gmail.com>
2018-01-31 17:51:44 +0000

    things to improve

--- 08-rails/review/21-30/30-has-many-through-forms-rails-lab/app/models/comment.rb
+++ 08-rails/review/21-30/30-has-many-through-forms-rails-lab/app/models/comment.rb
@@ -2,5 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user #=> comment.user
   belongs_to :post #=> comment.post
 
+  # TODO
+  # - you can add blank comments
+  # - throws exception if no user specfied
+  # - you can create duplicate users
   accepts_nested_attributes_for :user, reject_if: :all_blank
 end
