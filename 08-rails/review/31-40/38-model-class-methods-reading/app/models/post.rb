49d6119f9e0162d72c158d5b1be8a34a02b42997
theboymo <billfero@gmail.com>
2018-02-13 19:59:46 +0000

    refactor controller, encapsulate database sql queries into model class methods

--- 08-rails/review/31-40/38-model-class-methods-reading/app/models/post.rb
+++ 08-rails/review/31-40/38-model-class-methods-reading/app/models/post.rb
@@ -5,18 +5,19 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case
 
   def self.by_author(id)
-    author = Author.find(id)
-    unless author == nil
-      author.posts
-    end
+    # author = Author.find(id)
+    # unless author == nil
+    #   author.posts
+    # end
+    where(author: id)
   end
 
   def self.from_today
-
+    where("created_at >=?", Time.zone.today.beginning_of_day)
   end
 
   def self.old_news
-
+    where("created_at <?", Time.zone.today.beginning_of_day)
   end
 
   private
