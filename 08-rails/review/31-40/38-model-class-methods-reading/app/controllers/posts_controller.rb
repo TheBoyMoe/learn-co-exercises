7c252a33748442b7515a78d7d0fc3894be2b2684
theboymo <billfero@gmail.com>
2018-02-13 19:52:39 +0000

    refactor view, move filter logic into the controller

--- 08-rails/review/31-40/38-model-class-methods-reading/app/controllers/posts_controller.rb
+++ 08-rails/review/31-40/38-model-class-methods-reading/app/controllers/posts_controller.rb
@@ -1,9 +1,22 @@
 class PostsController < ApplicationController
-  # ensure the controller has access to the params hash
-  helper_method :params
+  # ensure the view has access to the params hash
+  # helper_method :params
 
   def index
-    @posts = Post.all
+    # filter the @posts list based on user input
+    if !params[:author].blank?
+      @posts = Post.where(author: params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      else
+        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
+    @authors = Author.all
   end
 
   def show
