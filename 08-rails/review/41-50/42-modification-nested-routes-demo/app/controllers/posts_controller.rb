da7e95e176068af0b44e48066bfbf6d4c7524cfd
theboymo <billfero@gmail.com>
2018-02-15 15:07:43 +0000

    initial commit

---
+++ 08-rails/review/41-50/42-modification-nested-routes-demo/app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+
+  def index
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
+  end
+
+  def show
+    if params[:author_id]
+      @post = Author.find(params[:author_id]).posts.find(params[:id])
+    else
+      @post = Post.find(params[:id])
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post))
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
