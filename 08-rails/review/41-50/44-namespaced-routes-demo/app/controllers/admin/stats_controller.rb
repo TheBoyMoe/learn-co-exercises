0ad4070af6ef8b7894c26d702f321f6dd7717ace
theboymo <billfero@gmail.com>
2018-02-16 17:27:02 +0000

    overview of using scopes, modules and namespaces in creating routes

--- 08-rails/review/41-50/44-namespaced-routes-demo/app/controllers/admin/stats_controller.rb
+++ 08-rails/review/41-50/44-namespaced-routes-demo/app/controllers/admin/stats_controller.rb
@@ -1,8 +1,17 @@
-class StatsController < ApplicationController
+class Admin::StatsController < ApplicationController
 
   def index
     @post_count = Post.count
     @authors_count = Author.count
     @last_post = Post.last
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
   end
 end
--- 08-rails/review/41-50/44-namespaced-routes-demo/config/routes.rb
+++ 08-rails/review/41-50/44-namespaced-routes-demo/config/routes.rb
@@ -7,4 +7,45 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+	# replace =>
+	# get '/stats', to: 'stats#index'
+
+	# 1st iteration - create a 'scope'
+	# scope '/admin' do
+	# 	resources :stats, only: [:index, :show, :new, :edit]
+	# end
+
+	# stats 			GET   /admin/stats(.:format)                       stats#index
+	# new_stat 		GET   /admin/stats/new(.:format)                   stats#new
+	# edit_stat 	GET   /admin/stats/:id/edit(.:format)              stats#edit
+	# stat 				GET   /admin/stats/:id(.:format)                   stats#show
+
+
+
+	# 2nd iteration - create a 'module'
+	# scope '/admin', module: 'admin' do
+	# 	resources :stats, only: [:index, :show, :new, :edit]
+	# end
+
+	# the routes will be handled by the stats controller in the Admin module
+	# stats       GET   /admin/stats(.:format)                       admin/stats#index
+	# new_stat    GET   /admin/stats/new(.:format)                   admin/stats#new
+	# edit_stat   GET   /admin/stats/:id/edit(.:format)              admin/stats#edit
+	# stat        GET   /admin/stats/:id(.:format)                   admin/stats#show
+
+
+
+	# 3rd iteration - create a 'namespace'
+	# create a namespace when you want to route with a module and use it's name as the url prefix
+	# use a namespace when the path prefix and module names match
+	namespace :admin do
+		resources :stats, only: [:index, :show, :new, :edit]
+	end
+
+	# admin_stats 			GET   /admin/stats(.:format)                       admin/stats#index
+	# new_admin_stat	 	GET   /admin/stats/new(.:format)                   admin/stats#new
+	# edit_admin_stat 	GET   /admin/stats/:id/edit(.:format)              admin/stats#edit
+	# admin_stat 				GET   /admin/stats/:id(.:format)                   admin/stats#show
+
 end
