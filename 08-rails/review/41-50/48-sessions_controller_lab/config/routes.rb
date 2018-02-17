89c9d402d4c31e9bcb33bee6394d1517014b8e48
theboymo <billfero@gmail.com>
2018-02-17 17:17:03 +0000

    impl basic session controller in rails

---
+++ 08-rails/review/41-50/48-sessions_controller_lab/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+	get '/logout' => 'sessions#new' # ADDED
+end
