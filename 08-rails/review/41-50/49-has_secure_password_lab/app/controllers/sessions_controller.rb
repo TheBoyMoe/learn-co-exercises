f19b4b7a3f214f5418b1263efdc5c09308525c05
theboymo <billfero@gmail.com>
2018-02-17 18:29:40 +0000

    basic implementation of rails login with sessions

---
+++ 08-rails/review/41-50/49-has_secure_password_lab/app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+	# render login form
+	def new
+	end
+
+	# log the user in
+	def create
+
+	end
+
+	# log the user out
+	def destroy
+
+	end
+end
---
+++ 08-rails/review/41-50/49-has_secure_password_lab/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+	# render the signup form
+	def new
+	end
+
+	# create the user & log them in
+	def create
+
+	end
+
+	# render user page on login/signup
+	def show
+
+	end
+end
