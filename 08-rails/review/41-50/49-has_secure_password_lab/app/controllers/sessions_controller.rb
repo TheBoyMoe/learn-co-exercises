f2092bca2770965c41b9ef117aa5782b89299447
theboymo <billfero@gmail.com>
2018-02-18 09:59:04 +0000

    complete sessions and users controllers

--- 08-rails/review/41-50/49-has_secure_password_lab/app/controllers/sessions_controller.rb
+++ 08-rails/review/41-50/49-has_secure_password_lab/app/controllers/sessions_controller.rb
@@ -21,11 +21,18 @@ class SessionsController < ApplicationController
 
 	# log the user in
 	def create
-
+		@user = User.find_by(name: params[:user][:name])
+		if @user && @user.authenticate(params[:user][:password])
+			session[:user_id] = @user.id
+			redirect_to user_path(@user), notice: "You've successfully logged in"
+		else
+			redirect_to new_session_path, alert: "You failed to login"
+		end
 	end
 
 	# log the user out
 	def destroy
-
+		session[:user_id] = nil
+		redirect_to new_session_path, notice: "You've successfully logged out"
 	end
 end
--- 08-rails/review/41-50/49-has_secure_password_lab/app/controllers/users_controller.rb
+++ 08-rails/review/41-50/49-has_secure_password_lab/app/controllers/users_controller.rb
@@ -7,11 +7,26 @@ class UsersController < ApplicationController
 
 	# create the user & log them in
 	def create
-
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to user_path(@user), notice: "You've successfully registered"
+		else
+			redirect_to new_user_path, alert: "You're attempt to register has failed"
+		end
 	end
 
 	# render user page on login/signup
 	def show
-
+		if session[:user_id]
+			@user = User.find(params[:id])
+		else
+			redirect_to new_session_path, alert: "You need to be logged in"
+		end
 	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :password, :password_confirmation)
+		end
 end
