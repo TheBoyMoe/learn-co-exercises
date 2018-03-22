b304c475a6a2d450f7190cbed485a4fe8ebc7753
theboymo <billfero@gmail.com>
2018-03-22 15:53:24 +0000

    review traversing nested objects and arrays

--- 09-javascript-review/notes/scratchpad.js
+++ 09-javascript-review/notes/scratchpad.js
@@ -1,5 +1,7 @@
 // practice
 
+// using callbacks
+
 var callback = (n,i)=>{
     if (n == 1){
         return `found a match, index ${i}`
@@ -27,3 +29,57 @@ var findMatch = (array, value, callback)=>{
     }
     return false
 }
+
+
+//////////////////////////////////////////////////////////////
+// Traversing nested objects
+
+const person = {
+    name: "Awesome Name",
+    occupation: {
+        title: "Senior Manager of Awesome",
+        yearsHeld: 2
+    },
+    pets: [{
+        kind: "dog",
+        name: "Fiona"
+    }, {
+        kind: "cat",
+        name: "Ralph"
+    }]
+}
+
+person['name'] // 'Awesome Name'
+person['occupation']['yearsheld'] // 2
+person['pets'][0]['name'] // 'Fiona'
+
+person.name // 'Awesome Name'
+person.occupation.yearsHeld // 2
+person.pets[0].name // 'Fiona'
+
+
+const collections = [1, [2, [4, [5, [6]], 3]]]
+
+collections
+//  [1, Array(2)]
+collections[0]
+// 1
+collections[1]
+//  [2, Array(3)]
+collections[1][0]
+// 2
+collections[1][1]
+// [4, Array(2), 3]
+collections[1][1][0]
+// 4
+collections[1][1][1]
+// [5, Array(1)]
+collections[1][1][2]
+//3
+collections[1][1][1][0]
+// 5
+collections[1][1][1][1]
+// [6]
+collections[1][1][1][1][0]
+// 6
+
