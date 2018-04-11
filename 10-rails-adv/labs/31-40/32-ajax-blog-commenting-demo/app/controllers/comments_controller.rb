560027325aabb611b875ccc5adba55962e74e29b
theboymo <billfero@gmail.com>
2018-04-11 16:27:04 +0100

    impl ajax request in rails using a generic request and remote true

--- 10-rails-adv/labs/31-40/32-ajax-blog-commenting-demo/app/controllers/comments_controller.rb
+++ 10-rails-adv/labs/31-40/32-ajax-blog-commenting-demo/app/controllers/comments_controller.rb
@@ -5,7 +5,19 @@ class CommentsController < ApplicationController
     @comments = @post.comments
     # ensure that the response to the ajax request is only the content and does not include the layout file
     # render 'comments/index', layout: false # responds with html
-    render json: @comments # response in json
+    
+    # render json: @comments # response in json
+    
+    # action renders index.html view or index.js if requested
+    # render layout: false # or render 'index.js', layout: false
+
+    # respond to either html/js requests - this is implicit in Rails
+    # ALL YOU NEED in 'render layout: false' in this example
+    # here you're stating it explicitly, allowing you to render other views
+    respond_to do |format|
+      format.html {render 'index.html', layout: false}
+      format.js {render 'index.js', layout: false}
+    end
   end
 
   private
