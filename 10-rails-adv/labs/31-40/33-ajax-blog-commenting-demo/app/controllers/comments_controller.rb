55ffeefff9f0424b18b4b81c654e9728ec314f08
theboymo <billfero@gmail.com>
2018-04-11 17:58:40 +0100

    add comments to post using a form_for helper

---
+++ 10-rails-adv/labs/31-40/33-ajax-blog-commenting-demo/app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def index
+    @comments = @post.comments
+     respond_to do |format|
+       format.html {render 'index.html', layout: false}
+       format.js {render 'index.js', layout: false}
+     end
+  end
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+  end
+
+  private
+    def set_post
+      @post = Post.find_by(id: params[:post_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+end
