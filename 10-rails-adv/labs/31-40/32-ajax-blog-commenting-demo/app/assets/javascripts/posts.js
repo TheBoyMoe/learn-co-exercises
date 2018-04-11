dba4262257049d124ce88985c712e4ae2d397685
theboymo <billfero@gmail.com>
2018-04-11 11:38:00 +0100

    make ajax calls to the server from client on click event

--- 10-rails-adv/labs/31-40/32-ajax-blog-commenting-demo/app/assets/javascripts/posts.js
+++ 10-rails-adv/labs/31-40/32-ajax-blog-commenting-demo/app/assets/javascripts/posts.js
@@ -1,18 +1,41 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
-  $('a.load_comments').on('click', function(e){
+$(function(){ // equivalent to document ready
+  $("a.load_comments").on('click', function(e){
+
+    comments_url = e.target.href // or you can use 'this.href'
+    
+    // $.ajax({
+    //   method: 'GET',
+    //   url: comments_url,
+    //   success: function(response){
+    //     $("div.comments").html(response);
+    //   },
+    //   error: function(error){
+    //     $("div.comments").html("<p>Error retrieving data from server!</p>");
+    //   }
+    // });
+
+    // .done(function(response){
+    //   // server responds with the page it would have rendered
+    //   console.log(response)
+    //   $("div.comments").html(response)
+    // })
+    // .error(function(error){
+    //   $("div.comments").html(<p>Error retrieving data from server!</p>)
+    // });
+    
+    // simpler tech using .get()
+    $.get(comments_url)
+      .success(function(response){
+        $("div.comments").html(response)
+      })
+      .error(function(error){
+        $("div.comments").html("<p>Error retrieving data from server!</p>")
+      });
+
     e.preventDefault();
-    url = e.target.href // or you can use 'this.href'
-    $.ajax({
-      method: 'GET',
-      url: url
-    })
-    // server response is passed to .done which executes a callback to process the response
-    .done(function(response){
-      // server responds with the page it would have rendered
-      console.log(response)
-    })
-  })
+  });
 })
+
