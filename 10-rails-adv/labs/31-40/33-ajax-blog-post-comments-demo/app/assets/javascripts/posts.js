91c2700eded19c3fb725a3c51b3030ef468dd5c7
theboymo <billfero@gmail.com>
2018-04-12 06:40:31 +0100

    amend the ajax cal ore generic

--- 10-rails-adv/labs/31-40/33-ajax-blog-post-comments-demo/app/assets/javascripts/posts.js
+++ 10-rails-adv/labs/31-40/33-ajax-blog-post-comments-demo/app/assets/javascripts/posts.js
@@ -1,21 +1,41 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+// $(function(){ // equivalent to document ready
+//   $("#new_comment").on('submit', function(e){
+//     comments_url = e.target.action // or this.action
+// 
+//     data = {
+//       'authenticity_token' : $("input[name='authenticity_token']").val(),
+//       'comment': {
+//         'content': $("#comment_content").val()
+//       }
+//     }
+//     
+//     $.ajax({
+//       type: 'POST',
+//       url: comments_url,
+//       data: data,
+//       success: function(response){
+//         $("#comment_content").val("") // clear textarea
+//         $("div.comments ul").append(response)
+//         $("input[type='submit']").removeAttr('disabled')
+//       }
+//     })
+// 
+//     e.preventDefault();
+//   });
+// })
+
+// making the form more generic
 $(function(){ // equivalent to document ready
   $("#new_comment").on('submit', function(e){
-    comments_url = e.target.action // or this.action
 
-    data = {
-      'authenticity_token' : $("input[name='authenticity_token']").val(),
-      'comment': {
-        'content': $("#comment_content").val()
-      }
-    }
-    
-    $.ajax({
-      type: 'POST',
-      url: comments_url,
-      data: data,
+   $.ajax({
+      type: ($("input[name='_method']").val() || this.method), // capture patch/put/delete or post
+      url: this.action,
+      data: $(this).serialize(), // serialize the form data
+      // the callback is the only part that is unique - this can be programmed on the server side too - use remote: true
       success: function(response){
         $("#comment_content").val("") // clear textarea
         $("div.comments ul").append(response)
@@ -26,4 +46,3 @@ $(function(){ // equivalent to document ready
     e.preventDefault();
   });
 })
-
